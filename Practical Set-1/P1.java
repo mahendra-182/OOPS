d7b947f5a5d070815f0a1128c01be8cd884f070a
Your Name <mahendrasuryavanshi3525@gmail.com>
2026-04-30 01:41:49 +0530

    Fix P1: remove dead Scanner line, format output to 2 decimal places as per question

--- Practical Set-1/P1.java
+++ Practical Set-1/P1.java
@@ -1,14 +1,11 @@
 import java.util.*;
 public class P1 {
-    public static void main(String args[]){
-        int n;
-        Scanner SC = new Scanner (System.in);
+    public static void main(String args[]) {
+        Scanner SC = new Scanner(System.in);
         System.out.print("ENTER DISTANCE IN METER :- ");
-        n=SC.nextInt();
-        float d;
-        System.out.print("DISTANCE IN FEET :- "+(n*3.28084));
-        d=SC.nextFloat();
-        
+        double n = SC.nextDouble();
+        double feet = n * 3.28084;
+        System.out.printf("DISTANCE IN FEET :- %.2f%n", feet);
     }
 }
 
